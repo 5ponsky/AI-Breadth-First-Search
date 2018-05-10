f8bcb7415a9aff6b15a431681e45ba5195e9b1e6
Liam Dillingham <mailgd9@gmail.com>
2018-05-10 13:19:52 -0500

    state

--- GameState.java
+++ GameState.java
@@ -3,10 +3,48 @@
 class GameState {
   GameState prev;
   byte[] state;
+  boolean[][] validity;
 
   GameState(GameState _prev) {
     prev = _prev;
     state = new byte[22];
+
+    //
+    validity = new boolean[10][10];
+
+    // true means that space is occupied or illegal
+    for(int i = 0; i < validity.length; ++i) {
+      validity[i][0] = true;
+      validity[i][9] = true;
+      validity[0][i] = true;
+      validity[9][i] = true;
+    }
+
+    // top left inner corner
+    validity[1][1] = true;
+    validity[1][2] = true;
+    validity[2][1] = true;
+
+    // bottom left inner corner
+    validity[7][1] = true;
+    validity[8][1] = true;
+    validity[8][2] = true;
+
+    // top right inner corner
+    validity[1][7] = true;
+    validity[1][8] = true;
+    validity[2][8] = true;
+
+    // bottom right inner corner
+    validity[7][8] = true;
+    validity[8][7] = true;
+    validity[8][9] = true;
+
+    // inner block
+    validity[4][3] = true;
+    validity[4][4] = true;
+    validity[3][4] = true;
+    printState();
   }
 
   static String stateToString(byte[] b) {
@@ -19,5 +57,15 @@ class GameState {
     }
 
     return sb.toString();
+  }
+
+  void printState() {
+    for(int i = 0; i < validity.length; ++i) {
+      for(int j = 0; j < validity[0].length; ++j){
+        int bool = validity[i][j] ? 1 : 0;
+        System.out.print(bool + "  ");
+      }
+      System.out.println("");
+    }
   }
 }
